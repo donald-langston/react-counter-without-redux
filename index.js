bc22e11ecab094e10a4a61a82a8368273b2cd8a3
Donald <donald_langston@yahoo.com>
2020-06-07 21:37:42 -0400

    Rendering counter and incrementing count

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            value: this.state.value + 1
+        })
+    }
+    render() {
+        return <div>
+            <span>{this.state.value}</span>
+            <button onClick={this.handleClick}>Click</button>
+        </div>
+    }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'))
